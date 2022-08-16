04de116caae70954270ad596964f9b3020fe24d4
macaroonie <nqn205@nyu.edu>
2022-08-15 23:54:28 -0400

    Add order model and my-books component along with my-book service

--- fire-library-spa/src/app/app.module.ts
+++ fire-library-spa/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { TopComponent } from './top/top.component';
 import { BooksComponent } from './books/books.component';
 import { TemplateComponent } from './template/template.component';
+import { MyBooksComponent } from './my-books/my-books.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TemplateComponent } from './template/template.component';
     HomeComponent,
     TopComponent,
     BooksComponent,
-    TemplateComponent
+    TemplateComponent,
+    MyBooksComponent
   ],
   imports: [
     BrowserModule,
