4623f7bf4ebd81f1866feda0bad29da6b8fec705
Abanoub Sadek <abanoubfetaha@Abanoubs-MacBook-Pro.local>
2022-08-16 19:15:09 -0700

    testing

---
+++ FireLibrary.Test/BooksControllerTest.cs
@@ -0,0 +1,149 @@
+﻿using Xunit;
+using Xunit.Sdk;
+using Moq;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using FireLibrary2.Models;
+using FireLibrary2.Controllers;
+using FireLibrary2.Data;
+using Microsoft.AspNetCore.Mvc;
+using FireLibrary2.DTOs;
+using Microsoft.Extensions.Logging;
+
+namespace FireLibrary.Test
+{
+    public class BooksControllerTest
+    {
+        [Fact]
+        public async Task GetBook_Input_Result()
+        {
+            BookDTO book = new BookDTO();
+            book.Isbn = "12345";
+            book.Pages = 200;
+            book.TotalCopies = 5;
+            book.AvalableCopies = 5;
+            book.Genre = "Genre";
+
+            Console.WriteLine("The Book test started");
+
+            var ser = JsonSerializer.Serialize(book);
+
+            Console.WriteLine(ser);
+
+
+
+            Mock<ILogger<BooksController>> mockLogger = new();
+            Mock<IRepository> mockRepo = new();
+
+            mockRepo.Setup(repo => repo.GetBookAsync("12345")).ReturnsAsync(book);
+
+            var cus = new BooksController(mockRepo.Object, mockLogger.Object);
+
+
+            //var actionResult = await cus.GetCustomer(12345) as OkObjectResult;
+            var result = await cus.GetBook("12345");
+
+            var resultContent = result.Result as ContentResult;
+            if (resultContent == null)
+            {
+                Console.WriteLine("Book Result Content is NULL");
+            }
+
+            var item = result.Value;
+
+            var contentResult = result as ActionResult<BookDTO>;
+
+            //var test = result.ToString() as CustomerDTO;
+            //CustomerDTO cus1 = result.Value as CustomerDTO;
+
+            Assert.Equal(book.Isbn, result.Value.Isbn);
+            //Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public async Task GetBook_BadInput_Notfound()
+        {
+            BookDTO book = new BookDTO();
+            book.Isbn = "12345";
+            book.Pages = 200;
+            book.TotalCopies = 5;
+            book.AvalableCopies = 5;
+            book.Genre = "Genre";
+
+            //Console.WriteLine("The Book test started");
+
+            var ser = JsonSerializer.Serialize(book);
+
+            Console.WriteLine(ser);
+
+
+
+            Mock<ILogger<BooksController>> mockLogger = new();
+            Mock<IRepository> mockRepo = new();
+
+            mockRepo.Setup(repo => repo.GetBookAsync("1234555")).ReturnsAsync(book);
+
+            var cus = new BooksController(mockRepo.Object, mockLogger.Object);
+
+
+            //var actionResult = await cus.GetCustomer(12345) as OkObjectResult;
+            var result = await cus.GetBook("12345");
+
+            var resultContent = result.Result as ContentResult;
+            if (resultContent == null)
+            {
+                Console.WriteLine("Book Result Content is NULL");
+            }
+
+            var item = result.Value;
+
+            var contentResult = result as ActionResult<BookDTO>;
+
+            //var test = result.ToString() as CustomerDTO;
+            //CustomerDTO cus1 = result.Value as CustomerDTO;
+
+            Assert.IsType<NotFoundResult>(result.Result);
+            //Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+
+        [Fact]
+        public async Task SearchBooksGenre_Input_Result()
+        {
+            List<BookDTO> book = new List<BookDTO>();
+            book.Add(new BookDTO { Isbn = "12345", Genre = "genre" });
+           
+            //Console.WriteLine("The Book test started");
+
+            var ser = JsonSerializer.Serialize(book);
+
+            Console.WriteLine(ser);
+
+            Mock<ILogger<BooksController>> mockLogger = new();
+            Mock<IRepository> mockRepo = new();
+
+            mockRepo.Setup(repo => repo.SearchBooksGenreAsync("genre")).ReturnsAsync(book);
+
+            var cus = new BooksController(mockRepo.Object, mockLogger.Object);
+
+            //var actionResult = await cus.GetCustomer(12345) as OkObjectResult;
+            var result = await cus.SearchBooksGenre("genre");
+
+            var resultContent = result.Result as ContentResult;
+            if (resultContent == null)
+            {
+                Console.WriteLine("Book Result Content is NULL");
+            }
+            var item = result.Value;
+
+            var contentResult = result as ActionResult<List<BookDTO>>;
+
+            //var test = result.ToString() as CustomerDTO;
+            //CustomerDTO cus1 = result.Value as CustomerDTO;
+
+            Assert.IsType<OkObjectResult>(result.Result);
+            //Assert.IsType<OkObjectResult>(result.Result);
+          
+        }
+    }
+}
--- FireLibrary.Test/CustomerControllerTest.cs
+++ FireLibrary.Test/CustomerControllerTest.cs
@@ -7,6 +7,11 @@ using FireLibrary2.Models;
 using FireLibrary2.Controllers;
 using FireLibrary2.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FireLibrary2.DTOs;
+using System.Net;
+using Castle.Core.Resource;
+//using System.Web.Http.Results; 
 
 namespace FireLibrary.Test;
 
@@ -15,42 +20,125 @@ public class CustomerControllerTest
     [Fact]
     public async Task GetCustomer_Input_Result()
     {
-        Customer customer = new Customer();
+        CustomerDTO customer = new CustomerDTO();
         customer.BookCount = 150;
-        customer.CanBorrow = true;
+        customer.Canborrow = true;
         customer.CustomerId = 12345;
         customer.Username = "username";
         customer.Fines = 20;
 
+        Console.WriteLine("The test started");
+
         var ser = JsonSerializer.Serialize(customer);
 
-        System.Console.WriteLine(ser);
+        Console.WriteLine(ser);
 
-        //string jsonShouldbe = @"{""CustomerId"":12345,""Username"":""username"",""CanBorrow"":true,""Fines"":20,""BookCount"":150,""Orders"":null}";
-        //Console.WriteLine(jsonShouldbe);
 
-        var mockRepo = new Mock<DbSet<Customer>>();
-        
-        mockRepo.Setup(repo => repo.FindAsync(12345)).ReturnsAsync(customer);
-  
 
-        Mock<DataContext> mockContext = new();
-        mockContext.Setup(a => a.Customers).Returns(mockRepo.Object);
-        var cus = new CustomersController(mockContext.Object);
+        Mock<ILogger<CustomersController>> mockLogger = new();
+        Mock<IRepository> mockRepo = new();
 
+        mockRepo.Setup(repo => repo.GetCustomerByIdAsync(12345)).ReturnsAsync(customer);
+
+        var cus = new CustomersController(mockRepo.Object, mockLogger.Object);
+
+
+        //var actionResult = await cus.GetCustomer(12345) as OkObjectResult;
         var result = await cus.GetCustomer(12345);
 
-        var resultContent = (ActionResult)result.Result;
-       
-        if(resultContent == null)
+        var resultContent = result.Result as ContentResult;
+        if (resultContent == null)
         {
             Console.WriteLine("Result Content is NULL");
         }
-        var item = result.Value;
-        Customer customer1 = item as Customer;
 
-        Assert.Equal(customer, customer1);
+        var item = result.Value;
+
+        var contentResult = result as ActionResult<CustomerDTO>;
+
+        //var test = result.ToString() as CustomerDTO;
+        //CustomerDTO cus1 = result.Value as CustomerDTO;
+
+        //Assert.Equal(customer.CustomerId, result.Value.CustomerId);
+        Assert.IsType<OkObjectResult>(result.Result);
     }
+
+    [Fact]
+    public async Task GetCustomer_BadInput_NotFound()
+    { 
+        CustomerDTO customer = new CustomerDTO();
+        customer.BookCount = 150;
+        customer.Canborrow = true;
+        customer.CustomerId = 12345;
+        customer.Username = "username";
+        customer.Fines = 20;
+
+        Console.WriteLine("The test started");
+
+        var ser = JsonSerializer.Serialize(customer);
+
+        Console.WriteLine(ser);
+
+
+
+        Mock<ILogger<CustomersController>> mockLogger = new();
+        Mock<IRepository> mockRepo = new();
+
+        mockRepo.Setup(repo => repo.GetCustomerByIdAsync(123445)).ReturnsAsync(customer);
+
+        var cus = new CustomersController(mockRepo.Object, mockLogger.Object);
+
+
+        //var actionResult = await cus.GetCustomer(12345) as OkObjectResult;
+        var result = await cus.GetCustomer(12345);
+
+        var resultContent = result.Result as ContentResult;
+        if (resultContent == null)
+        {
+            Console.WriteLine("Result Content is NULL");
+        }
+
+        var item = result.Value;
+
+        var contentResult = result as ActionResult<CustomerDTO>;
+
+        //var test = result.ToString() as CustomerDTO;
+        //CustomerDTO cus1 = result.Value as CustomerDTO;
+
+        //Assert.Equal(customer.CustomerId, result.Value.CustomerId);
+        Assert.IsType<NotFoundResult>(result.Result);
+     }
+ }
+
+
+    //Assert.IsType<ActionResult<CustomerDTO>>(result);
+
+    //Assert.IsType<OkObjectResult>(result);
+    //CustomerDTO result1 = CustomerDTO.CreateCustomerDTO(customer);
+
+
+    //Customer customer1 = item as Customer;
+    //Assert.Equal(ser, resultContent.Content );
+
+
+    //    Mock<DataContext> mockContext = new();
+    //    mockContext.Setup(a => a.Customers).Returns(mockRepo.Object);
+
+    //    var cus = new CustomersController(mockContext.Object);
+
+    //    var result = await cus.GetCustomer(12345);
+
+    //    var resultContent = (ActionResult)result.Result;
+
+    //    if(resultContent == null)
+    //    {
+    //        Console.WriteLine("Result Content is NULL");
+    //    }
+    //    var item = result.Value;
+    //    Customer customer1 = item as Customer;
+
+    //    Assert.Equal(customer, customer1);
+
     //*********************************************************************************************************************
     //[Fact]
     //public async Task PostCustomer_Input_Result()
@@ -132,4 +220,3 @@ public class CustomerControllerTest
 
     //    Assert.IsType<NoContentResult>(result);
     //}
-}
